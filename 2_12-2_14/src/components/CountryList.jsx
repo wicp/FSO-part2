11076291786eac6ab2f5b085332ae67c5e4e45a4
wicp <noneof@yourbusiness.com>
2021-03-30 21:01:34 +0100

    completed 2.13

--- 2_12-2_14/src/components/CountryList.jsx
+++ 2_12-2_14/src/components/CountryList.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Country from './Country'
 
 const CountryList = ({countries, filterText}) => {
+    const [expanded,setExpanded] = useState({})
     const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(filterText))
+
+    function expandButton(countryName) {
+        const newExpanded = {...expanded}
+        // undefined countries in expanded State are falsy, so we initialise them to true
+        newExpanded[countryName] = !expanded[countryName]
+        setExpanded(newExpanded)
+    }
+    
+
     if (countries.length === 0)
     return (
         <p>loading...</p>
@@ -18,7 +28,13 @@ const CountryList = ({countries, filterText}) => {
     else if (filteredCountries.length < 11)
     return (
         <ul>
-            {filteredCountries.map(country => <li key={country.name}>{country.name}</li>)}
+            {filteredCountries.map(country => 
+            <li key={country.name}>
+                {country.name}
+                <button onClick={() => expandButton(country.name)}>{expanded[country.name] ? 'Hide' : 'Show'}</button>
+                {expanded[country.name] ? <Country country={country} /> : null }
+            </li>
+            )}
         </ul>
     )
     else if (filteredCountries.length > 10)
